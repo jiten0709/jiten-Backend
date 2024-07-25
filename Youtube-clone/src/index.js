6e661be3a07feb871b8c57c91bd18c80cb1dbd93
jiten0709 <parmar.jiten.microsoft@gmail.com>
2024-07-25 20:47:04 +0530

    custom api response & error handling done

--- Youtube-clone/src/index.js
+++ Youtube-clone/src/index.js
@@ -30,11 +30,11 @@ dotenv.config({
 })
 
 connectDB()
-// .then(() => {
-//     app.listen(process.env.PORT || 8000, () => {
-//         console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
-//     })
-// })
-// .catch((err) => {
-//     console.log("src :: index.js :: MongoDB connection failed !!! ", err);
-// })
+.then(() => {
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+    })
+})
+.catch((err) => {
+    console.log("src :: index.js :: MongoDB connection failed !!! ", err);
+})
