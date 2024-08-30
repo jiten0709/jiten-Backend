f1b16802b9cc953e3b9b7923ad02afb8a4891047
jiten0709 <parmar.jiten.microsoft@gmail.com>
2024-08-30 16:54:01 +0530

    Refresh 'acessToken' done

--- Youtube-clone/src/controllers/user.controllers.js
+++ Youtube-clone/src/controllers/user.controllers.js
@@ -3,6 +3,7 @@ import { ApiError } from '../utils/ApiError.js'
 import { ApiResponse } from '../utils/ApiResponse.js'
 import { User } from '../models/user.models.js'
 import { uploadOnCloudinary } from "../utils/cloudinary.js";
+import jwt from "jsonwebtoken";
 
 const generateAccessAndRefreshTokens = async (userId) => {
     try {
@@ -160,8 +161,45 @@ const logoutUser = asyncHandler(async (req, res) => {
         .json(new ApiResponse(200, {}, 'user.controllers :: User logged out successfully'))
 })
 
+const refreshAccessToken = asyncHandler(async (req, res) => {
+    const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken
+    if (!incomingRefreshToken) {
+        throw new ApiError(401, 'user.controllers :: Unauthorized request')
+    }
+    try {
+        const decodedToken = jwt.verify(incomingRefreshToken, process.env.REFRESH_TOKEN_SECRET)
+        const user = await User.findById(decodedToken?._id)
+        if (!user) {
+            throw new ApiError(401, 'user.controllers :: Invalid refresh token')
+        }
+        if (incomingRefreshToken !== user?.refreshToken) {
+            throw new ApiError(401, 'user.controllers :: Refresh token is expired or used')
+        }
+        const options = {
+            httpOnly: true,
+            secure: true
+        }
+        const { accessToken, newRefreshToken } = await generateAccessAndRefreshTokens(user._id)
+
+        return res
+            .status(200)
+            .cookie('accessToken', accessToken, options)
+            .cookie('refreshToken', newRefreshToken, options)
+            .json(
+                new ApiResponse(
+                    200,
+                    { accessToken, refreshToken: newRefreshToken },
+                    'user.controllers :: Access token refreshed'
+                )
+            )
+    } catch (error) {
+        throw new ApiError(401, `user.controllers :: ${error?.message}` || 'user.controllers :: Invalid refresh token')
+    }
+})
+
 export {
     registerUser,
     loginUser,
-    logoutUser
+    logoutUser,
+    refreshAccessToken
 }
--- Youtube-clone/src/routes/user.routes.js
+++ Youtube-clone/src/routes/user.routes.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { upload } from '../middlewares/multer.middlewares.js'
 import { verifyJWT } from "../middlewares/auth.middlewares.js";
-import { loginUser, registerUser, logoutUser } from "../controllers/user.controllers.js";
+import { loginUser, registerUser, logoutUser, refreshAccessToken } from "../controllers/user.controllers.js";
 
 const router = Router()
 
@@ -22,5 +22,6 @@ router.route('/login').post(loginUser)
 
 // secured routes
 router.route('/logout').post(verifyJWT, logoutUser)
+router.route('/refresh-token').post(refreshAccessToken)
 
 export default router
